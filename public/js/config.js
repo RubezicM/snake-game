c74d68c62204236780b387ea89318a5df561f689
Miljan Rubezic <miljanrubezic@gmail.com>
2019-01-26 23:35:45 +0100

    Refactoring number one

---
+++ public/js/config.js
@@ -0,0 +1,13 @@
+const UI_CONFIG = {
+    borderCanvas: 20,
+    fieldHeight: canvasHeight + 5,
+    fieldWidth: canvasWidth + 5,
+    fieldColor: "#F7D800",
+    fieldBorderColor: "#171717",
+    snakeColor: "#3BD80D"
+  };
+  const GAME_CONFIG = {
+    snakeSpeed: 65,
+    snakePartSize: 10,
+    snakeLength: 5
+  };
