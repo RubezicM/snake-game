dcc9780d494709c38febd7b4e10b48ca2d9575c5
RubezicM <miljanrubezic@gmail.com>
2025-06-24 01:26:17 +0200

    remade main script to fix some bugs

--- public/js/main.js
+++ public/js/main.js
@@ -1,238 +1,45 @@
-const game = (function () {
-  const startGameBtn = document.getElementById('startGameBtn');
-  const mainPage = document.getElementById('game-panel');
-  const pausedPage = document.getElementById('paused-page');
+class SnakeGame {
+  constructor() {
+    this.canvas = document.getElementById('canvas');
+    this.ctx = this.canvas.getContext('2d');
+    this.mainPage = document.getElementById('game-panel');
+    this.pausedPage = document.getElementById('paused-page');
+    this.highscorePage = document.getElementById('highscores-page');
 
-  let canvas = document.getElementById('canvas');
-  let canvasWidth = 340;
-  let canvasHeight = 400;
-  canvas.width = canvasWidth;
-  canvas.height = canvasHeight;
+    this.canvasWidth = 340;
+    this.canvasHeight = 400;
+    this.canvas.width = this.canvasWidth;
+    this.canvas.height = this.canvasHeight;
 
-  const UI_CONFIG = {
-    fieldHeight: canvasHeight + 5,
-    fieldWidth: canvasWidth + 5,
-    fieldColor: '#222',
-    fieldBorderColor: '#171717',
-    snakeColor: 'white'
+    this.UI_CONFIG = {
+      fieldHeight: this.canvasHeight + 5,
+      fieldWidth: this.canvasWidth + 5,
+      fieldColor: '#222',
+      fieldBorderColor: '#171717',
+      snakeColor: 'white'
+    };
+
+    this.GAME_CONFIG = {
+      snakeSpeed: 65,
+      snakePartSize: 10,
+      snakeLength: 5
+    };
+
+    this.reset();
+    this.setupEventListeners();
+    this.initializeLocalStorage();
   }
 
-  const GAME_CONFIG = {
-    snakeSpeed: 65,
-    snakePartSize: 10,
-    snakeLength: 5
-  }
+  reset() {
+    this.currentDirection = 'right';
+    this.gameStarted = false;
+    this.blinkInterval = 0;
+    this.isPaused = false;
+    this.hitTheObstacle = false;
+    this.points = 0;
+    this.gameInterval = null;
 
-  let ctx = canvas.getContext('2d');
-  let snakeHead;
-  let currentDirection = 'right';
-  let gameStarted = false;
-  let blinkInterval = 0;
-  let isPaused = false;
-  let hitTheObstacle = false;
-  let points = 0;
-
-  // Game interval
-  document.getElementById('score').innerHTML = points
-  let game
-  let snake
-  let pray = {
-    x: 0,
-    y: 0
-  }
-
-  let clearCanvas = () => {
-    ctx.clearRect(0, 0, canvas.width, canvas.height)
-    ctx.fillStyle = UI_CONFIG.fieldColor
-    ctx.shadowColor = '#222'
-    ctx.shadowBlur = 5
-    ctx.fillRect(2.5, 0, UI_CONFIG.fieldWidth, UI_CONFIG.fieldHeight)
-    // ctx.fillStroke(ctx.strokeRect(20, 20, 150, 100));
-  };
-  let snakeBody = (x, y) => {
-    ctx.fillStyle = UI_CONFIG.snakeColor
-    ctx.fillRect(x, y, GAME_CONFIG.snakePartSize, GAME_CONFIG.snakePartSize)
-  };
-
-  let drawSnake = arr => {
-    for (let i = 0; i < arr.length; i++) {
-      if (i == 0) {
-        ctx.fillStyle = blinkInterval % 3 === 0 ? 'red' : UI_CONFIG.snakeColor
-        ctx.fillRect(
-          snake[0].x,
-          snake[0].y,
-          GAME_CONFIG.snakePartSize,
-          GAME_CONFIG.snakePartSize
-        );
-      } else {
-        snakeBody(arr[i].x, arr[i].y)
-      }
-    }
-  };
-
-  let prayBody = (x, y) => {
-    ctx.fillStyle = 'red';
-    ctx.fillRect(x, y, GAME_CONFIG.snakePartSize, GAME_CONFIG.snakePartSize)
-  };
-
-  let drawGame = () => {
-    collisionCheck(snakeHead)
-    if (!hitTheObstacle) {
-      clearCanvas()
-      prayBody(pray.x, pray.y)
-      moveSnake()
-      drawSnake(snake)
-    } else {
-      drawSnake(snake)
-      clearInterval(game)
-
-      //// Storing points to local storage
-      storeInLS(points)
-      setTimeout(() => {
-        displayScores()
-        highscorePage.style.display = 'block'
-        setTimeout(() => {
-          highscorePage.style.display = 'none'
-          gameInit()
-        }, 3000)
-      }, 1000)
-    }
-  };
-
-  let createPray = () => {
-    let _x =
-      Math.round(randomNum(0, canvasHeight) / GAME_CONFIG.snakePartSize) *
-      GAME_CONFIG.snakePartSize;
-    let _y =
-      Math.round(randomNum(0, canvasHeight) / GAME_CONFIG.snakePartSize) *
-      GAME_CONFIG.snakePartSize;
-    snake.forEach(part => {
-      const foodIsOnSnake = part.x == _x && part.y == _y;
-      const outOfBonds =
-        _x >= canvasWidth || _y >= canvasHeight || (_x == 0 || _y == 0);
-      if (foodIsOnSnake || outOfBonds) createPray();
-      else prayBody(_x, _y);
-      pray.x = _x;
-      pray.y = _y;
-    });
-  };
-
-
-  let collisionCheck = head => {
-    if (
-      head.x < 0 ||
-      head.y < 0 ||
-      (head.x >= UI_CONFIG.fieldWidth - 10 ||
-        head.y >= UI_CONFIG.fieldHeight - 10)
-    ) {
-
-      hitTheObstacle = true;
-    }
-  };
-
-  let selfCollisionCheck = head => {
-    for (let i = 0; i < snake.length; i++) {
-      if (i !== 0) {
-        if (snake[i].x === head.x && snake[i].y === head.y) {
-          hitTheObstacle = true;
-        }
-      }
-    }
-  }
-
-  let moveSnake = () => {
-    if (currentDirection == 'right') {
-      snakeHead = { x: snake[0].x + GAME_CONFIG.snakePartSize, y: snake[0].y }
-    } else if (currentDirection === 'up') {
-      snakeHead = { x: snake[0].x, y: snake[0].y - GAME_CONFIG.snakePartSize }
-    } else if (currentDirection === 'down') {
-      snakeHead = { x: snake[0].x, y: snake[0].y + GAME_CONFIG.snakePartSize }
-    } else if (currentDirection === 'left') {
-      snakeHead = { x: snake[0].x - GAME_CONFIG.snakePartSize, y: snake[0].y }
-    }
-    snake.unshift(snakeHead)
-    if (!eatingPray()) {
-      snake.pop()
-    } else {
-      points += 10
-      document.getElementById('score').innerHTML = points
-      createPray(snake)
-    }
-    selfCollisionCheck(snakeHead)
-  }
-  let eatingPray = () => {
-    let snakeHeadX = snake[0].x
-    let snakeHeadY = snake[0].y
-    if (snakeHeadX === pray.x && snakeHeadY === pray.y) {
-      return true
-    }
-  };
-
-  let directSnake = event => {
-    const leftKey = 37
-    const rightKey = 39
-    const upKey = 38
-    const downKey = 40
-    const space = 32
-    const keyPressed = event.keyCode
-
-    if (keyPressed == leftKey && currentDirection != 'right') {
-      currentDirection = 'left'
-    } else if (keyPressed == rightKey && currentDirection != 'left') {
-      currentDirection = 'right'
-    } else if (keyPressed == upKey && currentDirection != 'down') {
-      currentDirection = 'up'
-    } else if (keyPressed == downKey && currentDirection != 'up') {
-      currentDirection = 'down'
-    } else if (keyPressed == space) {
-      if (!gameStarted) {
-        mainPage.style.display = 'none'
-        gameInit()
-      } else {
-        pauseGame()
-      }
-
-    }
-  };
-
-  let pauseGame = () => {
-    if (hitTheObstacle) {
-      return false
-    }
-    isPaused === true ? (isPaused = false) : (isPaused = true)
-    if (isPaused) {
-      clearInterval(game)
-      pausedPage.style.display = 'flex'
-    } else {
-      pausedPage.style.display = 'none'
-      game = setInterval(gameMech, GAME_CONFIG.snakeSpeed)
-    }
-  };
-
-
-  // Getting directions for the snake
-
-  document.body.addEventListener('keydown', (e) => {
-    directSnake(e)
-  });
-
-  /***
-   * STORING EMPTY 5 PLACES IN LS
-   *
-   */
-  // Initial storing in a LS
-  storeInLS()
-
-
-  let gameInit = () => {
-    clearInterval(game)
-    gameStarted = true
-    isPaused = false
-    points = 0
-    document.getElementById('score').innerHTML = points
-    currentDirection = 'right'
-    hitTheObstacle = false
-    snake = [
+    this.snake = [
       { x: 150, y: 150 },
       { x: 140, y: 150 },
       { x: 130, y: 150 },
@@ -240,30 +47,244 @@ const game = (function () {
       { x: 110, y: 150 }
     ];
 
-    snakeHead = { x: 0, y: 0 }
-    clearCanvas()
-    drawSnake(snake)
-    createPray(snake)
-    moveSnake()
-    game = setInterval(gameMech, GAME_CONFIG.snakeSpeed)
-  };
+    this.snakeHead = { x: 150, y: 150 };
+    this.food = { x: 0, y: 0 };
 
-  function gameMech () {
-    if (gameStarted) {
-      blinkInterval++;
-      drawGame();
+    document.getElementById('score').innerHTML = this.points;
+  }
+
+  setupEventListeners() {
+    document.body.addEventListener('keydown', (e) => {
+      this.handleKeyPress(e);
+    });
+  }
+
+  initializeLocalStorage() {
+    if (typeof storeInLS === 'function') {
+      storeInLS();
     }
   }
 
-  function randomNum (min, max) {
-    return Math.floor(Math.random() * (max - min + 1) + min);
+  clearCanvas() {
+    this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
+    this.ctx.fillStyle = this.UI_CONFIG.fieldColor;
+    this.ctx.shadowColor = '#222';
+    this.ctx.shadowBlur = 5;
+    this.ctx.fillRect(2.5, 0, this.UI_CONFIG.fieldWidth, this.UI_CONFIG.fieldHeight);
   }
 
-  return {
-    gameInit,
-    mainPage,
-    points,
-    directSnake,
-    startGameBtn
+  drawSnakeBody(x, y, isHead = false) {
+    if (isHead) {
+      this.ctx.fillStyle = this.blinkInterval % 3 === 0 ? 'red' : this.UI_CONFIG.snakeColor;
+    } else {
+      this.ctx.fillStyle = this.UI_CONFIG.snakeColor;
+    }
+    this.ctx.fillRect(x, y, this.GAME_CONFIG.snakePartSize, this.GAME_CONFIG.snakePartSize);
   }
-})();
+
+  drawSnake() {
+    for (let i = 0; i < this.snake.length; i++) {
+      this.drawSnakeBody(this.snake[i].x, this.snake[i].y, i === 0);
+    }
+  }
+
+  drawFood() {
+    this.ctx.fillStyle = 'red';
+    this.ctx.fillRect(this.food.x, this.food.y, this.GAME_CONFIG.snakePartSize, this.GAME_CONFIG.snakePartSize);
+  }
+
+  createFood() {
+    let validPosition = false;
+    let attempts = 0;
+
+    while (!validPosition && attempts < 100) {
+      let x = Math.floor(Math.random() * (this.canvasWidth / this.GAME_CONFIG.snakePartSize)) * this.GAME_CONFIG.snakePartSize;
+      let y = Math.floor(Math.random() * (this.canvasHeight / this.GAME_CONFIG.snakePartSize)) * this.GAME_CONFIG.snakePartSize;
+
+      if (x >= 0 && x < this.canvasWidth && y >= 0 && y < this.canvasHeight) {
+        validPosition = true;
+
+        for (let part of this.snake) {
+          if (part.x === x && part.y === y) {
+            validPosition = false;
+            break;
+          }
+        }
+
+        if (validPosition) {
+          this.food.x = x;
+          this.food.y = y;
+        }
+      }
+
+      attempts++;
+    }
+
+    if (attempts >= 100) {
+      this.food.x = 200;
+      this.food.y = 200;
+    }
+  }
+
+  checkWallCollision(head) {
+    return head.x < 0 ||
+      head.y < 0 ||
+      head.x >= this.canvasWidth ||
+      head.y >= this.canvasHeight;
+  }
+
+  checkSelfCollision(head) {
+    for (let i = 1; i < this.snake.length; i++) {
+      if (this.snake[i].x === head.x && this.snake[i].y === head.y) {
+        return true;
+      }
+    }
+    return false;
+  }
+
+  moveSnake() {
+    let newHead = { ...this.snake[0] };
+
+    switch (this.currentDirection) {
+      case 'right':
+        newHead.x += this.GAME_CONFIG.snakePartSize;
+        break;
+      case 'left':
+        newHead.x -= this.GAME_CONFIG.snakePartSize;
+        break;
+      case 'up':
+        newHead.y -= this.GAME_CONFIG.snakePartSize;
+        break;
+      case 'down':
+        newHead.y += this.GAME_CONFIG.snakePartSize;
+        break;
+    }
+
+    this.snakeHead = newHead;
+
+    if (this.checkWallCollision(newHead) || this.checkSelfCollision(newHead)) {
+      this.hitTheObstacle = true;
+      return;
+    }
+
+    this.snake.unshift(newHead);
+
+    if (this.isEatingFood()) {
+      this.points += 10;
+      document.getElementById('score').innerHTML = this.points;
+      this.createFood();
+    } else {
+      this.snake.pop();
+    }
+  }
+
+  isEatingFood() {
+    return this.snake[0].x === this.food.x && this.snake[0].y === this.food.y;
+  }
+
+  gameLoop() {
+    if (!this.gameStarted || this.isPaused) return;
+
+    this.blinkInterval++;
+    this.moveSnake();
+
+    if (this.hitTheObstacle) {
+      this.endGame();
+      return;
+    }
+
+    this.clearCanvas();
+    this.drawFood();
+    this.drawSnake();
+  }
+
+  endGame() {
+    clearInterval(this.gameInterval);
+    this.gameInterval = null;
+
+    if (typeof storeInLS === 'function') {
+      storeInLS(this.points);
+    }
+
+    setTimeout(() => {
+      if (typeof displayScores === 'function') {
+        displayScores();
+      }
+      if (this.highscorePage) {
+        this.highscorePage.style.display = 'block';
+        setTimeout(() => {
+          this.highscorePage.style.display = 'none';
+          this.startGame();
+        }, 3000);
+      } else {
+        setTimeout(() => {
+          this.startGame();
+        }, 2000);
+      }
+    }, 1000);
+  }
+
+  handleKeyPress(event) {
+    const leftKey = 37;
+    const rightKey = 39;
+    const upKey = 38;
+    const downKey = 40;
+    const space = 32;
+    const keyPressed = event.keyCode;
+
+    if (keyPressed === space) {
+      event.preventDefault();
+      if (!this.gameStarted) {
+        this.mainPage.style.display = 'none';
+        this.startGame();
+      } else {
+        this.pauseGame();
+      }
+      return;
+    }
+
+    if (!this.gameStarted || this.isPaused || this.hitTheObstacle) return;
+
+    if (keyPressed === leftKey && this.currentDirection !== 'right') {
+      this.currentDirection = 'left';
+    } else if (keyPressed === rightKey && this.currentDirection !== 'left') {
+      this.currentDirection = 'right';
+    } else if (keyPressed === upKey && this.currentDirection !== 'down') {
+      this.currentDirection = 'up';
+    } else if (keyPressed === downKey && this.currentDirection !== 'up') {
+      this.currentDirection = 'down';
+    }
+  }
+
+  pauseGame() {
+    if (this.hitTheObstacle) return;
+
+    this.isPaused = !this.isPaused;
+
+    if (this.isPaused) {
+      clearInterval(this.gameInterval);
+      this.gameInterval = null;
+      this.pausedPage.style.display = 'flex';
+    } else {
+      this.pausedPage.style.display = 'none';
+      this.gameInterval = setInterval(() => this.gameLoop(), this.GAME_CONFIG.snakeSpeed);
+    }
+  }
+
+  startGame() {
+    if (this.gameInterval) {
+      clearInterval(this.gameInterval);
+    }
+
+    this.reset();
+    this.gameStarted = true;
+    this.createFood();
+    this.clearCanvas();
+    this.drawFood();
+    this.drawSnake();
+
+    this.gameInterval = setInterval(() => this.gameLoop(), this.GAME_CONFIG.snakeSpeed);
+  }
+}
+
+const game = new SnakeGame();
